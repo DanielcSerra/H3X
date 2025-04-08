3983b382f6206426895be08d58b1eb24a7819377
Duarte <2241735@my.ipleiria.pt>
2025-04-08 14:28:40 +0100

    Duarte Santos 08/04

---
+++ H3X/sobrenos.php
@@ -0,0 +1,12 @@
+<?php include 'require/head.php'; ?>
+<title>H3X - Pagina</title>
+<link rel="stylesheet" href="css/sobrenos.css">
+</head>
+
+<body>
+    <?php include 'require/navbar.php';?>
+    <div class="container">
+    <!-- Codigo Aqui -->
+    </div>
+</body>
+<?php include 'require/footer.php';?>
