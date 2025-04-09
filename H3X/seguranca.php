679e9862e88a0dfdfdbc5da349a0f315008477b9
DanielcSerra <2242571@my.ipleiria.pt>
2025-04-09 20:01:57 +0100

    Alexandre

---
+++ H3X/seguranca.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <?php include 'require/head.php'; ?>
+    <title>H3X - Segurança</title>
+    <link rel="stylesheet" href="css/seguranca.css">
+</head>
+<body>
+    <?php include 'require/navbar.php';?>
+    <div class="Banner">
+    <img src="img/BanSeguranca.png" alt="BannerSeguranca">
+    <h1>Segurança</h1>
+    </div>
+
+
+
+
+<section class="fundo">
+  <!-- Seção Segurança -->
+  <section class="text-center">
+    <h2 class="section-title">SEGURANÇA</h2>
+    <section class="container">
+        <div class="imagem">
+          <img src="img/Segurancaimg1.png" alt="Segurança" >
+        </div>
+        <div class="texto">
+          <p >
+            <span class="sub">.Proibido Fumar </span><br><br>
+            <span class="sub">.Proibido entrar com bebidas ou alimentos </span><br><br>
+            <span class="sub">.Proibido o uso de drogas ilícitas </span><br><br>
+
+          </p>
+        </div>
+    </div>
+  </section>
+
+  <!-- Seção Serviços -->
+  <section class=" cards text-white  text-center">
+    <h2 class="section-title">SERVIÇOS</h2>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="card1 col-md-6 mb-4">
+          <div class="border-container">
+            <div class="imgcard1">
+            <img src="img/Segurancaimg2.png"  alt="DJ">
+      </div>
+          </div>
+
+          <p class="textocard1">
+            <span>Oferecemos Auscutadores</span><br>
+            <p class="subtextocard1">Caso queira nos ofererecemos auscutadores<br> para as pessoas que desejarem<p>
+          </p>
+        </div>
+     
+        <div class="card2 col-md-6 mb-4">
+          <div class="custom-card">
+            <div class="card1 border-container">
+            <div class="imgcard2">
+              <img src="img/SegurancaIMG3.png" alt="Transporte">
+              </div>
+            </div>
+            <p  class="textocard2">
+              <span>Motoristas para levar a casa</span><br>
+              <p class="subtextocard1">Ofererecemos trasnporte privado por apenas 13.99 euros</p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</section>
+<?php include 'require/footer.php';?>
+</body>
+</html>
