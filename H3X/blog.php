e43c6932c30c5d82f400c0af3aa79b3182b93421
DanielcSerra <2242571@my.ipleiria.pt>
2025-05-11 00:11:55 +0100

    Daniel

---
+++ H3X/blog.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <?php include 'require/head.php'; ?>
+    <title>H3X - Arcade</title>
+    <link rel="stylesheet" href="css/blog.css">
+</head>
+
+<body>
+
+    <?php include 'require/navbar.php'; ?>
+
+    <main class="px-3 px-md-5">
+
+        <div class="container caixa-com-cantos mt-5 py-5 px-3 px-md-5 position-relative">
+
+            <h2 class="section-title mb-4">BLOG</h2>
+
+        </div>
+
+
+        <div class="container pb-5">
+            <div class="row g-4 justify-content-center text-center text-white">
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/imgtest.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Abertura do H3X em Portugal contou com uma noite inesquecível</h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/92003_lux.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Decoração do H3X para o Verão </h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/imgtest.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Abertura do H3X </h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/imgtest.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Abertura do H3X em Portugal contou com uma noite inesquecível</h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/imgtest.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Abertura do H3X em Portugal contou com uma noite inesquecível</h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/imgtest.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Abertura do H3X em Portugal contou com uma noite inesquecível</h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/imgtest.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Abertura do H3X em Portugal contou com uma noite inesquecível</h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                        <div class="destaque-img">
+                            <img src="img/imgtest.jpg" alt="Arcade Retro" />
+                        </div>
+                        <div class="card-text text-start mt-2">
+                            <h5 class="fw-bold mb-2">Abertura do H3X em Portugal contou com uma noite inesquecível</h5>
+                        </div>
+                        <div class="card-text text-start mb-2 mt-auto">
+                            <a href="#"><i class="ri-arrow-right-up-line"></i> IR PARA POST</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-3 d-flex justify-content-center">
+                    <div class="destaque-card">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
+    <?php include 'require/footer.php'; ?>
+</body>
+
+</html>
--- H3X/require/navbar.php
+++ H3X/require/navbar.php
@@ -21,7 +21,7 @@ $fileName = basename($_SERVER["SCRIPT_NAME"]);
     <a href="#">EVENTOS</a>
     <a href="#">VIP</a>
     <a href="#">GALERIA</a>
-    <a href="#">BLOG</a>
+    <a href="blog.php" class="<?= $fileName == 'blog.php' ? 'sel_nav' : '' ?>">BLOG</a>
     <a href="arcade.php" class="<?= $fileName == 'arcade.php' ? 'sel_nav' : '' ?>">ARCADE</a>
     <a href="comochegar.php" class="<?= $fileName == 'comochegar.php' ? 'sel_nav' : '' ?>">COMO CHEGAR</a>
     <a href="#">CONTACTOS</a>
