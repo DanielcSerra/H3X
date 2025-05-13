2d65453e210b66aecc2ed6780b7d1a1571a0285c
Duarte <2241735@my.ipleiria.pt>
2025-05-13 10:57:00 +0100

    Duarte
    small changes

---
+++ H3X/login.php
@@ -0,0 +1,34 @@
+<?php include 'require/head.php'; ?>
+<title>H3X - Album</title>
+<link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+<?php include 'require/navbar.php';?>
+<div class="container mt-5">
+    <h1 class="text-white">Login</h1>
+    <form>
+  <div class="row mb-3">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="First name">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Last name">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="First name">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Last name">
+    </div>
+  </div>
+</form>
+</div>
+<div class="container mt-5">
+  <h2>ola o meu nome é jorge</h2>
+  <p>fbsdufvuasdvfuiasydbuiysdbvuyasdbvousydbvoysdbvoyiasdfoiyasdvysadbvosabdfosajhdfbasdobfsaydbfoa</p>
+</div>
+</body>
+<?php include 'require/footer.php';?>
