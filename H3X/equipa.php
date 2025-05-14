0f2558e5b25c1f6c62c1823f84f2eae78c10dc08
Recky <rodrigoferreira0407@gmail.com>
2025-05-14 13:24:01 +0100

    rodri

---
+++ H3X/equipa.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <?php include 'require/head.php'; ?>
+    <title>H3X - Arcade</title>
+    <link rel="stylesheet" href="css/equipa.css">
+</head>
+
+<body>
+
+    <?php include 'require/navbar.php'; ?>
+    <div class="hero d-flex justify-content-center align-items-center text-center position-relative">
+        <h1>ARCADE</h1>
+    </div>
+    <?php include 'require/footer.php'; ?>
+</body>
+
+</html>
