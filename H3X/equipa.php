377a4fe46d514c1a584f1c50e4d643fd1030e3e9
Recky <rodrigoferreira0407@gmail.com>
2025-05-20 14:50:23 +0100

    rodri

--- H3X/equipa.php
+++ H3X/equipa.php
@@ -3,36 +3,15 @@
 
 <head>
     <?php include 'require/head.php'; ?>
-<<<<<<< HEAD
     <title>H3X - Arcade</title>
-=======
-    <title>H3X - Equipa</title>
->>>>>>> 464f6d35776e80c4e5d3cb3da389b3ee87e37aa6
     <link rel="stylesheet" href="css/equipa.css">
 </head>
 
 <body>
-
     <?php include 'require/navbar.php'; ?>
     <div class="hero d-flex justify-content-center align-items-center text-center position-relative">
         <h1>A EQUIPA</h1>
     </div>
-<<<<<<< HEAD
-
-    <div class="Titulo">
-        <h1>FUNDADORES E GESTÃO STAFF</h1>
-    </div>
-    <div class="Part1">
-        <div class="imagem1">
-            <img src="img/FUNDADOR.png" alt=fundador>
-            <h2>FUNDADOR</h2>
-        </div>
-
-
-
-
-    </div>
-=======
     <div class="titulo">
         <h1>FUNDADORES E GESTÃO <span class="staff">STAFF</span></h1>
     </div>
@@ -47,14 +26,14 @@
         </div>
     </div>
     <div class="areastaff">
+        <img src="img/before.png" width="100%" alt="barra">
+        <img src="img/Retangulo21.png" width="100%" alt="branco">
         <div class="titulostaff">
             <h1>STAFF</h1>
         </div>
-        <img src="img/before.png" width="100%" alt="barra">
-        <img src="img/Retangulo21.png" width="100%" alt="branco">
     </div>
+    
 
->>>>>>> 464f6d35776e80c4e5d3cb3da389b3ee87e37aa6
     <?php include 'require/footer.php'; ?>
 </body>
 
