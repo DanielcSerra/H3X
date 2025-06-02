baa0e9eb7e231e1433192910b6b4f27c3cfc5d9f
Recky <rodrigoferreira0407@gmail.com>
2025-06-02 21:04:09 +0100

    rodri
    asasax

--- H3X/db_config.php
+++ H3X/db_config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "Ferreira2016";
 $database = "h3x";
 
 $conn = new mysqli($servername, $username, $password, $database);
